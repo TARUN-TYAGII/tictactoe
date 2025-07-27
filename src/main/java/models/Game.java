a707fbd24333c51cc1be6c4b99dbcfb816e27d78
TARUN TYAGI <shivatyagii@outook.com>
2025-07-27 23:31:44 +0530

    tic tac toe

---
+++ src/main/java/models/Game.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private Board board;
+    private List<Player> players = new ArrayList<>();
+    private GameStatus gameStatus;
+}
